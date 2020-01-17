2109bda59c108039c8b0358118132ed969b5e3e9
zeekers63 <gavz63@uw.edu>
2020-01-16 17:59:26 -0800

    getting close but he searches too far and freezes the browser

--- js/agent.js
+++ js/agent.js
@@ -1,17 +1,17 @@
 var GAMEBOARD = [];
 
-var getXY = function(x, y) {
-    var i = Math.floor((x - BUBBLES_X_START + BUBBLES_GAP/2)/BUBBLES_GAP);
-    var j = Math.floor((y - BUBBLES_Y_START + 9)/17.75);
+var getXY = function (x, y) {
+    var i = Math.floor((x - BUBBLES_X_START + BUBBLES_GAP / 2) / BUBBLES_GAP);
+    var j = Math.floor((y - BUBBLES_Y_START + 9) / 17.75);
 
     return {x: i, y: j}
 };
 
 var buildGameboard = function () {
     GAMEBOARD = [];
-    for(var i = 0; i < 26; i++) {
+    for (var i = 0; i < 26; i++) {
         GAMEBOARD.push([]);
-        for(var j = 0; j < 29; j++) {
+        for (var j = 0; j < 29; j++) {
             GAMEBOARD[i].push({
                 bubble: false,
                 superBubble: false,
@@ -25,8 +25,8 @@ var buildGameboard = function () {
         }
     }
 
-    for(var i = 0; i < BUBBLES_ARRAY.length; i++) {
-        var bubbleParams = BUBBLES_ARRAY[i].split( ";" );
+    for (var i = 0; i < BUBBLES_ARRAY.length; i++) {
+        var bubbleParams = BUBBLES_ARRAY[i].split(";");
         var y = parseInt(bubbleParams[1]) - 1;
         var x = parseInt(bubbleParams[2]) - 1;
         var type = bubbleParams[3];
@@ -36,24 +36,24 @@ var buildGameboard = function () {
         } else {
             GAMEBOARD[x][y].superBubble = true;
         }
-        if(eaten === 0) {
+        if (eaten === 0) {
             GAMEBOARD[x][y].eaten = false;
         } else {
             GAMEBOARD[x][y].eaten = true;
         }
     }
 
-    for(var i = 0; i < 26; i++) {
-        for(var j = 0; j < 29; j++) {
-            if(!GAMEBOARD[i][j].bubble && !GAMEBOARD[i][j].superBubble){
+    for (var i = 0; i < 26; i++) {
+        for (var j = 0; j < 29; j++) {
+            if (!GAMEBOARD[i][j].bubble && !GAMEBOARD[i][j].superBubble) {
                 GAMEBOARD[i][j] = null;
             }
         }
     }
 
-    for(var i = 0; i < 26; i++) {
-        for(var j = 0; j < 29; j++) {
-            if((i === 0 && (j === 13)) ||
+    for (var i = 0; i < 26; i++) {
+        for (var j = 0; j < 29; j++) {
+            if ((i === 0 && (j === 13)) ||
                 (i === 1 && (j === 13)) ||
                 (i === 2 && (j === 13)) ||
                 (i === 3 && (j === 13)) ||
@@ -76,7 +76,7 @@ var buildGameboard = function () {
                 (i === 22 && (j === 13)) ||
                 (i === 23 && (j === 13)) ||
                 (i === 24 && (j === 13)) ||
-                (i === 25 && (j === 13)))  {
+                (i === 25 && (j === 13))) {
                 GAMEBOARD[i][j] = {
                     bubble: false,
                     superBubble: false,
@@ -91,13 +91,13 @@ var buildGameboard = function () {
         }
     }
 
-    var p = getXY(GHOST_INKY_POSITION_X,GHOST_INKY_POSITION_Y);
-    if(p.x >= 0 && p.x < 26 && GAMEBOARD[p.x][p.y]) GAMEBOARD[p.x][p.y].inky = true;
-    p = getXY(GHOST_PINKY_POSITION_X,GHOST_PINKY_POSITION_Y);
+    var p = getXY(GHOST_INKY_POSITION_X, GHOST_INKY_POSITION_Y);
+    if (p.x >= 0 && p.x < 26 && GAMEBOARD[p.x][p.y]) GAMEBOARD[p.x][p.y].inky = true;
+    p = getXY(GHOST_PINKY_POSITION_X, GHOST_PINKY_POSITION_Y);
     if (p.x >= 0 && p.x < 26 && GAMEBOARD[p.x][p.y]) GAMEBOARD[p.x][p.y].pinky = true;
-    p = getXY(GHOST_BLINKY_POSITION_X,GHOST_BLINKY_POSITION_Y);
+    p = getXY(GHOST_BLINKY_POSITION_X, GHOST_BLINKY_POSITION_Y);
     if (p.x >= 0 && p.x < 26 && GAMEBOARD[p.x][p.y]) GAMEBOARD[p.x][p.y].blinky = true;
-    p = getXY(GHOST_CLYDE_POSITION_X,GHOST_CLYDE_POSITION_Y);
+    p = getXY(GHOST_CLYDE_POSITION_X, GHOST_CLYDE_POSITION_Y);
     if (p.x >= 0 && p.x < 26 && GAMEBOARD[p.x][p.y]) GAMEBOARD[p.x][p.y].clyde = true;
 
     p = getXY(PACMAN_POSITION_X, PACMAN_POSITION_Y);
@@ -105,25 +105,25 @@ var buildGameboard = function () {
 
 };
 
-function drawRect(i,j) {
+function drawRect(i, j) {
     var ctx = PACMAN_CANVAS_CONTEXT;
     var ygap = 17.75;
-    var x = BUBBLES_X_START + i*BUBBLES_GAP - BUBBLES_GAP/2;
-    var y = BUBBLES_Y_START + j*ygap- 9;
+    var x = BUBBLES_X_START + i * BUBBLES_GAP - BUBBLES_GAP / 2;
+    var y = BUBBLES_Y_START + j * ygap - 9;
     var w = BUBBLES_GAP;
     var h = ygap;
 
-    if(GAMEBOARD && GAMEBOARD[0] && GAMEBOARD[i][j]){
+    if (GAMEBOARD && GAMEBOARD[0] && GAMEBOARD[i][j]) {
         ctx.strokeStyle = "green";
-        ctx.rect(x,y,w,h);
+        ctx.rect(x, y, w, h);
         ctx.stroke();
     }
 }
 
 function drawDebug() {
-    for(var i = 0; i < 26; i++) {
-        for(var j = 0; j < 29; j++) {
-            drawRect(i,j);
+    for (var i = 0; i < 26; i++) {
+        for (var j = 0; j < 29; j++) {
+            drawRect(i, j);
         }
     }
 }
@@ -133,69 +133,104 @@ function selectMove() {
     buildGameboard();
 
     if (!PACMAN_DEAD && !GAMEOVER) { // make sure the game is running
-
+        var found = false;
         var xy = getXY(PACMAN_POSITION_X, PACMAN_POSITION_Y);
-        var x = xy.x;
-        var y = xy.y;
 
-        console.log("(" + x + ", " + y + ")");
+        var visited = [xy];
+        var queue = [];
+        var directions = getDirections(xy.x, xy.y, visited);
+        //Set up the queue with direction and adjacent cells
+        directions.forEach(function (dir) {
+            if (dir === 1) { //Right
+                var newNode = {x: xy.x + 1, y: xy.y, dir: dir};
+                queue.push(newNode);
+                visited.push(newNode);
+            } else if (dir === 2) { //Down
+                var newNode = {x: xy.x, y: xy.y + 1, dir: dir};
+                queue.push(newNode);
+                visited.push(newNode);
+            } else if (dir === 3) { //Left
+                var newNode = {x: xy.x - 1, y: xy.y, dir: dir};
+                queue.push(newNode);
+                visited.push(newNode);
+            } else { //Up
+                var newNode = {x: xy.x, y: xy.y - 1, dir: dir};
+                queue.push(newNode);
+                visited.push(newNode);
+            }
+        });
 
-        var directions = getDirections(x, y);
-        movePacman(lookForBubble(x, y, directions, 1).dir);
+        //BFS
+        while (!found) {
+            var node = queue.shift();
+            if (node) {
+                if (((GAMEBOARD[node.x][node.y].bubble || GAMEBOARD[node.x][node.y].superBubble) && !GAMEBOARD[node.x][node.y].eaten) ||
+                    (GAMEBOARD[node.x][node.y].pinky && GHOST_PINKY_AFFRAID_STATE) ||
+                    (GAMEBOARD[node.x][node.y].inky && GHOST_INKY_AFFRAID_STATE) ||
+                    (GAMEBOARD[node.x][node.y].blinky && GHOST_BLINKY_AFFRAID_STATE) ||
+                    (GAMEBOARD[node.x][node.y].clyde && GHOST_CLYDE_AFFRAID_STATE)) {
+                    found = true;
+                    movePacman(node.dir);
+                } else {
+                    directions = getDirections(node.x, node.y, visited);
+                    directions.forEach(function (dir) {
+                        if (dir === 1) { //
+                            var newNode = {x: node.x + 1, y: node.y, dir: node.dir};
+                            queue.push(newNode);
+                            visited.push(newNode);
+                        } else if (dir === 2) { //Down
+                            var newNode = {x: node.x, y: node.y + 1, dir: node.dir};
+                            queue.push(newNode);
+                            visited.push(newNode);
+                        } else if (dir === 3) { //Left
+                            var newNode = {x: node.x - 1, y: node.y, dir: node.dir};
+                            queue.push(newNode);
+                            visited.push(newNode);
+                        } else { //Up
+                            var newNode = {x: node.x, y: node.y - 1, dir: node.dir};
+                            queue.push(newNode);
+                            visited.push(newNode);
+                        }
+                    });
+                }
+            } else {
+                movePacman(oneDirection());
+            }
+        }
     }
 }
 
-function getDirections(x, y) {
+/**
+ * Gets directions pacman can move from the given x, y coordinate
+ * @param x, the x coordinate of the cell we are checking
+ * @param y, the y coordinate of the cell we are checking
+ * @param visited, an array of x, y coordinates that we have already tested.
+ * @returns directions, an array of (numbers) directions pacman can move from this coordinate
+ */
+function getDirections(x, y, visited) {
     var directions = [];
-    gameboard_x = GAMEBOARD[x];
-    if (GAMEBOARD[x + 1] && GAMEBOARD[x + 1][y]) { //Right
+    if (!visited.includes({x: x + 1, y: y}) &&
+        GAMEBOARD[x + 1] && GAMEBOARD[x + 1][y] &&
+        !GAMEBOARD[x + 1][y].pinky) { //Right
         directions.push(1);
     }
-    if (gameboard_x && gameboard_x[y + 1]) { //Down
+    if (!visited.includes({x: x, y: y + 1}) &&
+        GAMEBOARD[x] && GAMEBOARD[x][y + 1] &&
+        !GAMEBOARD[x][y + 1].pinky){ //Down
         directions.push(2);
+        visited.push({x: x, y: y + 1});
     }
-    if (GAMEBOARD[x - 1] && GAMEBOARD[x - 1][y]) { //Left
+    if (!visited.includes({x: x - 1, y: y}) &&
+        GAMEBOARD[x - 1] && GAMEBOARD[x - 1][y] &&
+        !GAMEBOARD[x - 1][y].pinky) { //Left
         directions.push(3);
+        visited.push({x: x - 1, y: y});
     }
-    if (gameboard_x && gameboard_x[y - 1]) { //Up
+    if (!visited.includes({x: x, y: y - 1}) &&
+        GAMEBOARD[x] && GAMEBOARD[x][y - 1] &&
+        !GAMEBOARD[x][y - 1].pinky) { //Up
         directions.push(4);
+        visited.push({x: x, y: y - 1});
     }
     return directions;
-}
-
-function lookForBubble(x, y, directions, count) {
-    //If there's a bubble adjacent to (x, y) return the direction from (x,y) that the bubble is
-    var dir = checkAdjacent(x, y, directions);
-    if (dir != null) return {dir: dir, count: count};
-
-    var right = getDirections(x + 1, y);
-    var down = getDirections(x, y + 1);
-    var left = getDirections(x - 1, y);
-    var up = getDirections(x, y - 1);
-
-    count++;
-    var paths = [];
-    paths.push(lookForBubble(x + 1, y, right, count).count);
-    paths.push(lookForBubble(x, y + 1, down, count).count);
-    paths.push(lookForBubble(x - 1, y, left, count).count);
-    paths.push(lookForBubble(x, y - 1, up, count).count);
-
-    return paths.indexOf(Math.min(...paths)) + 1;
-}
-
-function checkAdjacent(x, y, directions) {
-    var gameboard_x = GAMEBOARD[x];
-    if (directions.includes(1) && GAMEBOARD[x + 1] && !GAMEBOARD[x + 1][y].eaten) { //Right
-        //console.log(GAMEBOARD[x + 1][y]);
-        return 1;
-    } else if (directions.includes(2) && gameboard_x && !gameboard_x[y + 1].eaten) { //Down
-        //console.log(gameboard_x[y + 1]);
-        return 2;
-    } else if (directions.includes(3) && GAMEBOARD[x - 1] &&!GAMEBOARD[x - 1][y].eaten) { //Left
-        //console.log(GAMEBOARD[x - 1][y]);
-        return 3;
-    } else if (directions.includes(4) && gameboard_x && !gameboard_x[y - 1].eaten) { //Up
-        //console.log(gameboard_x[y - 1]);
-        return 4;
-    }
 }
